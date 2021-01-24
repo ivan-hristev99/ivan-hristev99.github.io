4d0f9306f607d9697d9963559f019b4d6190fc5d
ivan-hristev99 <iv.hristev99@gmail.com>
2021-01-24 19:07:17 +0000

    Update map location

--- js/scripts.js
+++ js/scripts.js
@@ -255,7 +255,7 @@ function initMap() {
 }
 
 function initBBSRMap() {
-    var la_fiesta = {lat: 20.305826, lng: 85.85480189999998};
+    var la_fiesta = {lat: 50.79453659881844, lng: -2.7060123069827267};
     var map = new google.maps.Map(document.getElementById('map-canvas'), {
         zoom: 15,
         center: la_fiesta,
